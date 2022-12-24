6fec467ea0fa82b0faf60ca3cb71c7e043e783ba
satriajun <114549025+satriajun@users.noreply.github.com>
2022-12-24 21:18:00 +0700

    Create Mahasiswa.java

---
+++ Mahasiswa.java
@@ -0,0 +1,31 @@
+package PBO1Sesi9;
+
+public class Mahasiswa<A,B,C>{
+    private A nim;
+    private B name;
+    private C clas;
+
+    public A getNim(){
+        return nim;
+    }
+
+    public void setNim(A nim){
+        this.nim=nim;
+    }
+
+    public B getName(){
+        return this.name;
+    }
+
+    public void setName(B name){
+        this.name=name;
+    }
+
+    public C getClas(){
+        return clas;
+    }
+
+    public void setClas(C clas){
+        this.clas=clas;
+    }
+}
