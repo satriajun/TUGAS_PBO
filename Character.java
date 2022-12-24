ac46d16db3c7a74088fe4921c4f9890f0814c52c
satriajun <114549025+satriajun@users.noreply.github.com>
2022-12-24 21:20:11 +0700

    Create Character.java

---
+++ Character.java
@@ -0,0 +1,9 @@
+package PBO2Sesi9.Deque;
+
+public abstract class Character {
+    abstract String getName();
+    abstract float getHp();
+    abstract float getAttack();
+    abstract float getDefense();
+    public abstract void infoCharacter();
+}
